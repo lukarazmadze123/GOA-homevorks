f98e9fe775b20b5ed6c381b72f4ba74b8cd07c2e
lukarazmadze123 <razmadzeluka399@gmail.com>
2024-06-29 22:30:18 -0700

    second commit

---
+++ day3/cw.py
@@ -0,0 +1,19 @@
+#დავალება 1
+
+brawlstaraaccaunt=1
+print(brawlstaraaccaunt)
+
+
+#დავალება2
+
+myage=10
+pliusage=10
+print(myage+pliusage)
+
+
+
+#დავალება3
+
+name="luka "
+numb1=5
+print(name*numb1)
---
+++ day3/hw.py
@@ -0,0 +1,17 @@
+#დავალება1
+
+goa="GOA "
+numb=1000
+print(goa*numb)
+
+#დავალება2
+
+monkey=55
+money_banana=5
+print(monkey/money_banana)
+
+#დავალება3
+
+yar=2013
+dayyar=2024
+print(dayyar-yar)
