fcf6d7cfa9bea7c29930429470b411d101c9a76d
lukarazmadze123 <razmadzeluka399@gmail.com>
2024-10-05 04:38:54 -0700

    secondes commit

---
+++ day27/hw/hw.py
@@ -0,0 +1,39 @@
+#N1
+
+#?
+
+#N2
+
+name="სალამი"
+print(name[::-1])
+
+#N3
+
+name="გამარჯობა"
+print(name[::2])
+
+#N4
+
+#?
+
+#N5
+
+name="programing"
+print(name[7:])
+
+#N6
+
+def aboutme():
+    print("andria " + "janezashvili " "11 ")
+
+aboutme("x")
+
+#N7
+
+def mathematics():
+    print(5 + 5)
+    print(5 - 2)
+    print(5 * 4)
+    print(5 / 5)
+
+mathematics("x")
