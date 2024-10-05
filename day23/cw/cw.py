6b9baf409a99f579732df2fec69e94db72150171
lukarazmadze123 <razmadzeluka399@gmail.com>
2024-10-04 22:22:28 -0700

    secondes commit

---
+++ day23/cw/cw.py
@@ -0,0 +1,7 @@
+qartuli_sufris_list = ["ხაში", "ქათამი", "შაურმა", "ხინკალი", "მწვადი"]
+qartuli_sufris_list[0] = ["ლობიო"]
+qartuli_sufris_list[1] = ["ლობიო"]
+qartuli_sufris_list[2] = ["ლობიო"]
+qartuli_sufris_list[3] = ["ლობიო"]
+qartuli_sufris_list[4] = ["ლობიო"]
+print(qartuli_sufris_list)
