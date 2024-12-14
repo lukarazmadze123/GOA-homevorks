c62fa8e6528f8b95b5d2299781b947af7835a593
lukarazmadze123 <razmadzeluka399@gmail.com>
2024-12-14 00:34:20 -0800

    second commit

---
+++ day 22/cw/cw.py
@@ -0,0 +1,8 @@
+# 1) სიის შექმნა და ელემენტების შეცვლა
+qartuli_sufris_list = ["ხაში", "ქათამი", "შაურმა", "ხინკალი", "მწვადი", "საზამთრო"]
+
+# ყველა ელემენტის შეცვლა "ლობიო"-თ
+qartuli_sufris_list = ["ლობიო"] * len(qartuli_sufris_list)
+
+# სიის დაბეჭდვა
+print(qartuli_sufris_list)
