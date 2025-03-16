1e63233b97be7986fceca771fb598ab54132a862
lukarazmadze123 <razmadzeluka399@gmail.com>
2025-03-16 05:19:31 -0700

    second commit

--- day 75/cw/cw3.py
+++ day 75/cw/cw3.py
@@ -1,1 +1,16 @@
-+
+class AceVentura:
+    def __init__(self):
+        self.hit_count = 0
+    
+    def stone_throw(self):
+        self.hit_count += 1
+        if self.hit_count == 1:
+            print("ვაიმე პირდაპირ ფეხში მომხვდა!")
+        elif self.hit_count == 2:
+            print("ვაიმე ორივე ფეხში მომხვდა!!~")
+
+# გამოყენება
+ace = AceVentura()
+ace.stone_throw()  # პირველად სროლა
+ace.stone_throw()  # მეორედ სროლა
+ace.stone_throw()  # მესამედ სროლა, არაფერი არ ხდება
