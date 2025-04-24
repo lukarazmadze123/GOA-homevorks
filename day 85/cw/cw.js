a36f2bffe661a222c223ab57c7baeeb402a3f95c
lukarazmadze123 <razmadzeluka399@gmail.com>
2025-04-24 07:54:39 -0700

    second commit

---
+++ day 85/cw/cw.js
@@ -0,0 +1,47 @@
+//1
+const body = document.getElementById("body")
+
+
+function lomi(){
+    body.style.color = "white"
+
+
+}
+
+const anybody = document.getElementById("body")
+
+
+//2
+function lomi(){
+    body.style.color = "white"
+    body.style.background = "black"
+
+
+}
+
+const gela = document.getElementById("body")
+
+
+function vefxvi(){
+    body.style.background = "white"
+    body.style.color = "black"
+}
+
+//3
+
+console.log("logika")
+
+//4
+
+let c="gela"
+    c=tina
+
+//5
+
+function lomi(){
+    body.style.color = "white"
+
+
+}
+
+const nobody = document.getElementById("body")
